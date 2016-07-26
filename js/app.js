7f0cd72bb43fa8f46a90a9a7de6ccb71894a3a54
rstiyer <rachel.stiyer@gmail.com>
2016-07-26 10:38:24 -0500

    Basic implementation, without relative messages

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+var secret, guessCount;
 
 $(document).ready(function(){
 	
@@ -12,6 +13,92 @@ $(document).ready(function(){
   		$(".overlay").fadeOut(1000);
   	});
 
+  	newGame();
+
+  	$('.new').click(function() {
+  		newGame();
+  	});
+
+  	$('.game > form').submit(function(e) {
+  		e.preventDefault();
+  		var userInput = getUserInput();
+  		if (isValidInput(userInput)) {
+  			recordGuess(userInput);
+  			updateFeedback(userInput);
+  		} else {
+  			alert('Please choose an integer between 1 and 100, inclusive.')
+  		}
+  		$('form')[0].reset();
+  	});
+
 });
 
+function newGame() {
+	setSecret();
+	resetCount();
+}
 
+function setSecret() {
+	secret = Math.floor(Math.random()*100 + 1);
+}
+
+function resetCount() {
+	guessCount = 0;
+	updateCount();
+}
+
+function increaseCount() {
+	guessCount++;
+	updateCount();
+}
+
+function updateCount() {
+	$('#count').text(guessCount);
+}
+
+function recordGuess(userInput) {
+	increaseCount();
+	addGuessToSavedList(userInput);
+}
+
+function addGuessToSavedList(userInput) {
+	$('ul#guessList').append('<li>'+ userInput +'</li>');
+}
+
+function isValidInput(userInput) {
+	return userInput && (0 < userInput) && (userInput < 101);
+}
+
+function getUserInput() {
+	var rawInput = $('input#userGuess').val();
+	return parseInt(rawInput);
+}
+
+function updateFeedback(userInput) {
+	var distance = calculateDistanceFromSecret(userInput);
+	var feedbackMsg = temperatureFeedback(distance);
+	displayFeedback(feedbackMsg);
+}
+
+function calculateDistanceFromSecret(userInput) {
+	return Math.abs(userInput - secret);
+}
+
+function temperatureFeedback(distance) {
+	if (distance >= 50)
+		return "Ice Cold";
+	else if (distance >= 30)
+		return "Cold";
+	else if (distance >= 20)
+		return "Warm";
+	else if (distance >= 10)
+		return "Hot";
+	else if (distance >= 1)
+		return "Very Hot";
+	else
+		return "You got it!"
+}
+
+function displayFeedback(feedbackMsg) {
+	$('#feedback').text(feedbackMsg);
+}
